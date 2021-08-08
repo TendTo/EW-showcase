5920cc5abbe53aeb87ac44db214d8724e93340a7
TendTo <casablancaernesto@gmail.com>
2021-08-09 01:04:15 +0200

    update(app): AppFooter component add i18

--- app/src/components/AppFooter/AppFooter.tsx
+++ app/src/components/AppFooter/AppFooter.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import ew_logo from '../../asset/img/ew-logo-small.png';
 import './AppFooter.css';
 
 
 function AppFooter() {
+    const { t } = useTranslation();
 
     return (
         <div className="footer-container">
             <a href="https://www.energyweb.org"><img src={ew_logo} alt="EW logo" /></a>
-            <div>All names, logos, images, and brands are property of their respective owners.</div>
+            <div>{t("FOOTER.LEGAL")}</div>
             <div >© 2021</div>
             <div>
                 <a href="https://github.com/TendTo">TendTo</a>
