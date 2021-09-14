56ce2d8353cd79a1c8c05a4e7d272738d3247453
TendTo <casablancaernesto@gmail.com>
2021-09-14 02:13:04 +0200

    test(app): update DIDEdit tests

--- app/src/components/DIDEdit/DIDEdit.test.tsx
+++ app/src/components/DIDEdit/DIDEdit.test.tsx
@@ -1,8 +1,23 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import DIDEdit from './DIDEdit';
 
-test('renders DIDEdit component', () => {
-    const { container } = render(<DIDEdit did="testDid" profile={undefined} setProfileClaims={(_) => {}}/>);
-    const elemnt = container.getElementsByClassName('fa fa-pencil');
-    expect(elemnt).toHaveLength(1);
+describe('DIDEdit component', () => {
+
+    test('renders DIDEdit component', () => {
+        const { container } = render(<DIDEdit did="testDid" profile={undefined} setProfileClaims={(_) => { }} />);
+        const element = container.getElementsByClassName('fa fa-pencil');
+        const titleElement = screen.queryByText(/^DID.EDIT_TITLE$/);
+
+        expect(element).toHaveLength(1);
+        expect(titleElement).toBeNull();
+    });
+
+    test('renders DIDEdit modal', () => {
+        const { container } = render(<DIDEdit did="testDid" profile={undefined} setProfileClaims={(_) => { }} />);
+        const element = container.getElementsByClassName('fa fa-pencil');
+        fireEvent.click(element[0]);
+        const titleElement = screen.getByText(/^DID.EDIT_TITLE$/i);
+
+        expect(titleElement).toBeInTheDocument();
+    });
 });
